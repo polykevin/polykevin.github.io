a62f9561f6132893362d93ce833c6f1c02f00a6f
Kevin <polykevin@outlook.fr>
2022-05-07 21:01:28 +0400

    added save and started writing the line code

--- dessin/script.js
+++ dessin/script.js
@@ -6,15 +6,20 @@ const colorEl = document.getElementById('color');
 const effacerEl = document.getElementById('effacer');
 var gommeBtn = document.getElementById('gomme');
 var crayonBtn = document.getElementById('crayon');
+var ligneBtn = document.getElementById('ligne')
+var saveBtn = document.getElementById('save')
 
 const ctx = canvas.getContext('2d');
 
 let taille = 0
 let appuyer = false
 colorEl.value = 'black'
-let color = colorEl.value
-let x
-let y
+let color = colorEl.value;
+let x;
+let y;
+
+function test2 () {
+    color = 'black'
 /* detecter si la souris est appuyé */
 canvas.addEventListener('mousedown', (e) => {
     appuyer = true
@@ -30,6 +35,7 @@ document.addEventListener('mouseup', (e) => {
     y = undefined
 })
 /* detecter si la souris est en mouvement */
+
 canvas.addEventListener('mousemove', (e) => {
     if(appuyer) {
         const x2 = e.offsetX
@@ -42,6 +48,55 @@ canvas.addEventListener('mousemove', (e) => {
         y = y2
     }
 })
+}
+
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png")
+    let a = document.createElement("a")
+    a.href = data
+    // what ever name you specify here
+    // the image will be saved as that name
+    a.download = "sketch.png"
+    a.click()
+})
+
+function test(){
+
+    function startDragLine(e) {
+        firstClick = [e.offsetX, e.offsetY];
+        //start the loop
+        intervalLoop = setInterval(function(){
+            
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+           
+            ctx.beginPath();
+            ctx.moveTo(firstClick[0], firstClick[1]);
+            ctx.lineTo(cursorX, cursorY, 6);
+    
+            ctx.strokeStyle = '#000000';
+            ctx.stroke();
+        },10);
+    }
+    function stopDragLine(){
+        clearInterval(intervalLoop);
+    }
+    
+    function init() {
+        document.onmousemove = function(e){
+            cursorX = e.offsetX;
+            cursorY = e.offsetY;
+        };
+        canvas.addEventListener('mousedown', startDragLine, false);
+        canvas.addEventListener('mouseup', stopDragLine, false);
+    }
+    var cursorX;
+    var cursorY;
+    var firstClick = [0,0];
+    var intervalLoop = null;
+    init();
+}
+
+
 
 function dessineCercle(x, y) {
     ctx.beginPath();
@@ -86,6 +141,8 @@ colorEl.addEventListener('change', (e) => color = e.target.value)
 
 gommeBtn.addEventListener('click',  () => color = 'white')
 
-crayonBtn.addEventListener('click',  () => color = 'black')
+ligneBtn.addEventListener('click',  () => test())
+
+crayonBtn.addEventListener('click',  () => test2())
 
 effacerEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
